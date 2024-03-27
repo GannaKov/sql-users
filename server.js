c532b39f56ec3a71d6a6861e232926c8b6d282d5
GannaKov <zlatta2000@gmail.com>
2024-03-27 16:53:26 +0100

    port env

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require("./app");
-
+require("dotenv").config();
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>
   console.log(`Server running. Use our API on port: ${PORT}`)
